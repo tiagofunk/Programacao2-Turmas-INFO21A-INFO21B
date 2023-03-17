6f9c6249900e7c639fff3755a55e837ef4ad2b0a
Tiago Funk <tiagoff.tf@gmail.com>
2023-03-17 14:03:37 -0300

    Primeira aula sobre revisão de JavaScript

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("olá")
+// var idade = 27
+// var altura = 1.85
+// var nome = 'tiago'
+// var e_professor = true
+// var nan = NaN
+
+// let nome = prompt("Digite seu nome")
+// alert( "prazer em te conhecer " + nome )
+
+// let numero1 = parseInt( prompt("Digite o primeiro número:") )
+// let numero2 = parseInt( prompt("Digite o segundo número:") )
+// var soma = numero1 + numero2
+// alert( "soma: " + soma )
+// alert( "Os números " + numero1 + " e " + numero2 + " somados são " + soma k)
+// alert( `Os números ${numero1} e ${numero2} somados são ${soma}`)
+
+var a = 5**2
+var x = 5
+x += 1
+x++
+
+var media = 6.5
+var situacao = media >= 7 ? "Aprovado" : "Reprovado"
+alert( situacao )
