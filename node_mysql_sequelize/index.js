df422777ad9e91ffbc053188acc414b46df96961
Tiago Funk <tiagoff.tf@gmail.com>
2023-11-21 18:03:11 -0300

    Correção no cors na versão no node mysql sequelize

--- node_mysql_sequelize/index.js
+++ node_mysql_sequelize/index.js
@@ -5,6 +5,13 @@ const pessoa = require("./pessoa")
 const app = express()
 app.use(express.json())
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+  });
+
 banco.conexao.sync( function(){
     console.log("Banco de dados conectado.");
 })
